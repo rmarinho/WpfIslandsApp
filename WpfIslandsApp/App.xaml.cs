66e7d41a89155deb7e9ab7b3aee1b56e24d63bda
Rui Marinho <me@ruimarinho.net>
2025-07-31 00:20:25 +0100

    Add debug info

--- WpfIslandsApp/App.xaml.cs
+++ WpfIslandsApp/App.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.UI.Xaml.Markup;
 using System.Configuration;
 using System.Data;
+using System.Resources;
 using System.Windows;
 
 namespace WpfIslandsApp
@@ -24,16 +25,28 @@ namespace WpfIslandsApp
 		override protected void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
 		{
 			this.Resources.MergedDictionaries.Add(new Microsoft.UI.Xaml.Controls.XamlControlsResources());
-		}
+			this.Resources.MergedDictionaries.Add(new Microsoft.UI.Xaml.ResourceDictionary
+				{
+					Source = new Uri("ms-appx:///Microsoft.Maui/Platform/Windows/Styles/Resources.xbf")
+				});
+
+			//this.Resources.MergedDictionaries.Add(new Microsoft.UI.Xaml.ResourceDictionary
+			//	{
+			//		Source = new Uri("ms-appx:///Platform/Windows/CollectionView/ItemsViewStyles.xbf")
+			//	});
+
+        }
 
 		IXamlType IXamlMetadataProvider.GetXamlType(string fullName)
 		{
-			return _xamlMetaDataProvider.GetXamlType(fullName);
+			var xamlType = _xamlMetaDataProvider.GetXamlType(fullName);
+            return xamlType;
 		}
 
 		IXamlType IXamlMetadataProvider.GetXamlType(System.Type type)
 		{
-			return _xamlMetaDataProvider.GetXamlType(type);
+			var xamlType = _xamlMetaDataProvider.GetXamlType(type);
+            return xamlType;
 		}
 
 		XmlnsDefinition[] IXamlMetadataProvider.GetXmlnsDefinitions()
